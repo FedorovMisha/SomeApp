f8fc1e9203248c6792ea0a455f4847252048e9d2
fedorov_misha <wands180@gmail.com>
2021-05-17 12:28:00 +0300

    Initial

---
+++ SomeApp.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeApp.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
